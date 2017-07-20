9a75b970d8e43f8a80504381e58b72deed41ece5
danweatherbee <dan@jedi-clan.us>
2017-07-20 13:06:11 -0700

    feat: new nav.

--- src/containers/site-navbar.js
+++ src/containers/site-navbar.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {navPageSelect} from '../actions/index'
+import {navPageSelect} from '../actions/index';
+import '../javascripts/lib/jquery-2.2.3.min.js';
+import '../javascripts/bootstrap/collapse.js';
+
 /*
  * We need "if(!this.props.links)" because we set state to null by default
  * */
@@ -9,18 +12,12 @@ const linkStyle = {
     textTransform: "uppercase"
 }
 
-const navStyle = {
-    position: "fixed",
-    zIndex: 1,
-    width: "100%"
-}
-
 class NavLinks extends Component {
 
     renderLinks() {
         return this.props.links.map((link) => {
             return(
-                        <li key={link.page}><a style={linkStyle} href={link.page} onClick={() => this.props.navPageSelect(link)}>{link.name}</a></li>
+                        <li className="links" key={link.page}><a style={linkStyle} href={link.page} onClick={() => this.props.navPageSelect(link)}>{link.name}</a></li>
                 );
         });
     }
@@ -30,14 +27,24 @@ class NavLinks extends Component {
         }
             return(
 
-                    <nav className="navbar navbar-default" style={navStyle}>
+                    <nav className="navbar navbar-inverse navbar-fixed-top">
                       <div className="container-fluid">
                         <div className="navbar-header">
-                          <a className="navbar-brand" href="#">Dan Weatherbee FENDeveloper</a>
+                              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                              </button>
+                          <a className="navbar-brand" href="#">
+                            Dan Weatherbee
+                           </a>
+
                         </div>
-                        <ul className="nav navbar-nav">
-                            {this.renderLinks()}
-                        </ul>
+                        <div className="collapse navbar-collapse" id="myNavbar">
+                            <ul className="nav navbar-nav">
+                                {this.renderLinks()}
+                            </ul>
+                      </div>
                       </div>
                     </nav>
 
