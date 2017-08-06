f694d1beed5ea05cb5b862974310f90b4ccbf8fd
danweatherbee <dan@jedi-clan.us>
2017-08-06 00:14:13 -0700

    feat: bridged the two apis to get top videos.

--- src/containers/youtube-container.js
+++ src/containers/youtube-container.js
@@ -7,20 +7,38 @@ const style = {
     backgroundColor: "black",
     marginTop: "1%"
 }
-
+let count = 0;
 const iframeStyle = {
     width: "100%",
-    height: "319px"
+    height: "600px"
 }
 class YouTube extends Component {
-
 
     render() {
 
             return(
                         <div>
-                        <iframe style={iframeStyle} src={this.props.api[150][0].video} ></iframe>
+
+                        {this.props.api[150].map((item) => {
+
+                            if (count > 49) {
+                                return
+                            }
+                            count++
+                            if (item.video === "https://www.youtube.com/embed/undefined") {
+                                return
+                            }
+                            return (
+
+                            <div>
+                            <iframe style={iframeStyle} src={item.video} ></iframe>
+                            </div>
+
+                            )
+                        })}
+
                         </div>
+
                 );
     }
 }
